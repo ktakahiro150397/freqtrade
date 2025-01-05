460ca6dc16767bcd385d1fce0f6a6284fc5df2b4
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2025-01-05 08:29:28 +0000

    何もわからないがバックテストの実行はOK

--- scripts.sh
+++ scripts.sh
@@ -1,7 +1,17 @@
 # Download candle data
-freqtrade download-data --pairs BTC/USDT ETH/USDT XRP/USDT --timerange 20190101-20231231 --timeframes 5m
+freqtrade download-data --pairs BTC/USDT ETH/USDT XRP/USDT --timerange 20190101-20231231 --timeframes 1m 5m　15m15m
 
 # Backtest
 freqtrade backtesting --strategy SampleStrategy --timerange 20190101-20190201 --timeframe 5m
-freqtrade backtesting --strategy MyStrategy --timerange 20190101-20190201 --timeframe 5m
+freqtrade backtesting --strategy MyStrategy --timerange 20190101-20190201 --timeframe 1m
 
+# Install WebUI
+freqtrade install-ui
+
+# Run bot
+# Web UI : http://localhost:8080/
+freqtrade trade -s SampleStrategy -s MyStrategy
+freqtrade trade --strategy MyStrategy
+
+# Run webserver
+freqtrade webserver
--- user_data/strategies/my_strategy.py
+++ user_data/strategies/my_strategy.py
@@ -1,33 +1,38 @@
-from freqtrade.strategy import IStrategy
-from pandas import DataFrame
 import talib.abstract as ta
+from pandas import DataFrame
+
+from freqtrade.strategy import IStrategy
+
 
 class MyStrategy(IStrategy):
+    timeframe = "15m"
 
     # set the initial stoploss to -10%
-    stoploss = -0.10
+    stoploss = -0.05
 
     # exit profitable positions at any time when the profit is greater than 1%
-    minimal_roi = {"0": 0.01}
+    minimal_roi = {"0": 0.05}
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         # generate values for technical analysis indicators
-        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
+        dataframe["rsi"] = ta.RSI(dataframe, timeperiod=14)
 
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         # generate entry signals based on indicator values
-        dataframe.loc[
-            (dataframe['rsi'] < 30),
-            'enter_long'] = 1
+        dataframe.loc[(dataframe["rsi"] < 30), ["enter_long", "enter_tag"]] = (1, "buy_signal_rsi")
+
+        # 動作確認用に、常にロングエントリーする
+        # dataframe["enter_long"] = 1
 
         return dataframe
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         # generate exit signals based on indicator values
-        dataframe.loc[
-            (dataframe['rsi'] > 70),
-            'exit_long'] = 1
+        dataframe.loc[(dataframe["rsi"] > 70), ["exit_long", "exit_tag"]] = (1, "exit_rsi")
+
+        # 動作確認用に、最新以外のロングポジションをクローズする
+        # dataframe.loc[dataframe["close"].shift(-1).notna(), "exit_long"] = 1
 
         return dataframe
